692accb45ae923c4a09176751b49cdf24853b36b
URK21CS3001MATHEW <109952093+URK21CS3001MATHEW@users.noreply.github.com>
2022-07-25 16:36:32 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,34 +1,27 @@
 import os
 class Stack:
-    def __init__(self, size):
+    def _init_(self, size):
         self.items = []
         self.size = size
 
     def is_empty(self):
-        self.top=-1
-        return self.top==-1
-        
+        return len(self.items) == 0
+
     def is_full(self):
-        return self.top=self.size-1
+        return len(self.items) == size
 
     def push(self, data):
         if not self.is_full():
-            self.top+=1
-            x=int(input("Enter the data to be inserted:"))
-            self.items[self.top]=x
-        
-            
+            self.items.append(values[1])
 
     def pop(self):
         if not self.is_empty():
-            print("the element deleted is:", self.elements[self])
-            self.items[slef.top]=None
-            self.top-=1
-            
+            self.items.pop()
 
     def status(self):
-        for i in range(self.top+1):
-            print(self.items[i])
+        for i in self.items:
+            print(i)
+
 # Do not change the following code
 size, queries = map(int, input().rstrip().split())
 stack = Stack(size)
